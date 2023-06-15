4b7b6be2898a0b47f0594ce3da3630a5a1ab758e
XenBG <wildbeastbg@gmail.com>
2023-06-16 00:55:28 +0300

    fix draggable cards component

--- components/BaseDraggableCards.vue
+++ components/BaseDraggableCards.vue
@@ -1,33 +1,13 @@
 <template>
     <div class="sub-categories">
-        <div
-            ref="container"
-            :class="['wrapper', itemsCount > 4 ? 'draggable' : null]"
-            @mousedown="onMouseDown"
-            @mousemove="onMouseMove"
-            @mouseup="onMouseUp"
-            @touchstart="onTouchStart"
-            @touchmove="onTouchMove"
-            @touchend="onTouchEnd"
-            @contextmenu="onContextMenu"
-        >
-            <BaseButton
-                v-if="itemsCount > 4"
-                :disabled="!canScrollLeft"
-                :title="t('general.buttons.scrollLeft')"
-                @click="scrollContainer(-0.4)"
-            >
+        <div ref="container" class="wrapper">
+            <BaseButton :disabled="!canScrollLeft" :title="t('general.buttons.scrollLeft')" @click="scrollContainer(-0.4)">
                 <template #icon-left>
                     <i class="fi fi-rr-arrow-small-left" />
                 </template>
             </BaseButton>
             <slot />
-            <BaseButton
-                v-if="itemsCount > 4"
-                :disabled="!canScrollRight"
-                :title="t('general.buttons.scrollRight')"
-                @click="scrollContainer(0.4)"
-            >
+            <BaseButton :disabled="!canScrollRight" :title="t('general.buttons.scrollRight')" @click="scrollContainer(0.4)">
                 <template #icon-left>
                     <i class="fi fi-rr-arrow-small-right" />
                 </template>
@@ -55,75 +35,31 @@
 
     const container = ref<HTMLElement | null>(null)
     const state = reactive({
-        isDragging: false,
-        startX: 0,
-        currentX: 0,
+        /**
+         * The current scroll position of the container
+         *
+         * @type Number
+         */
         scrollPosition: 0,
+        /**
+         * The maximum scroll position of the container
+         *
+         * @type Number
+         */
+        maxScrollPosition: 0,
+        /**
+         * The width of the container
+         *
+         * @type Number
+         */
         containerWidth: 0
     })
 
-    const onMouseMove = (event: MouseEvent) => {
-        if (state.isDragging && container.value) {
-            state.currentX = event.pageX - container.value.offsetLeft
-            const diff = state.currentX - state.startX
-            container.value.scrollLeft = state.scrollPosition - diff
-        }
-    }
-
-    const onMouseUp = () => {
-        state.isDragging = false
-        if (container.value) {
-            state.scrollPosition = container.value.scrollLeft
-        }
-        document.removeEventListener('mousemove', onMouseMove)
-        document.removeEventListener('mouseup', onMouseUp)
-    }
-
-    const onMouseDown = (event: MouseEvent) => {
-        if (event.button === 2 || event.target instanceof HTMLAnchorElement) {
-            return // Skip if right mouse button is clicked or target is a link
-        }
-        state.isDragging = true
-        state.startX = event.pageX - container.value!.offsetLeft
-        document.addEventListener('mousemove', onMouseMove)
-        document.addEventListener('mouseup', onMouseUp)
-    }
-
-    const onTouchMove = (event: TouchEvent) => {
-        if (state.isDragging && container.value) {
-            state.currentX = event.touches[0].pageX - container.value.offsetLeft
-            const diff = state.currentX - state.startX
-            container.value.scrollLeft = state.scrollPosition - diff
-            // Prevent page scrolling while dragging
-            event.preventDefault()
-        }
-    }
-
-    const onTouchEnd = () => {
-        state.isDragging = false
-        if (container.value) {
-            state.scrollPosition = container.value.scrollLeft
-        }
-        // Remove touchmove event listeners
-        container.value!.removeEventListener('touchmove', onTouchMove)
-        document.body.removeEventListener('touchmove', onTouchMove)
-    }
-
-    const onTouchStart = (event: TouchEvent) => {
-        if (event.target instanceof HTMLAnchorElement) {
-            return // Skip if target is a link
-        }
-        state.isDragging = true
-        state.startX = event.touches[0].pageX - container.value!.offsetLeft
-        // Prevent page scrolling while dragging
-        container.value!.addEventListener('touchmove', onTouchMove)
-        document.body.addEventListener('touchmove', onTouchMove, { passive: false })
-    }
-
-    const onContextMenu = (event: MouseEvent) => {
-        event.preventDefault() // Prevent the context menu from appearing
-    }
-
+    /**
+     * Scroll the container by a percentage of its width
+     *
+     * @param percentage - The percentage to scroll (positive for right, negative for left)
+     */
     const scrollContainer = (percentage: number) => {
         if (!container.value) return
         const scrollAmount = Math.floor(state.containerWidth * percentage)
@@ -147,6 +83,14 @@
         }
     }
 
+    /**
+     * Smoothly scroll the element to the specified scroll position
+     *
+     * @param element - The element to scroll
+     * @param scrollLeft - The target scroll position
+     * @param duration - The duration of the scroll animation in milliseconds
+     * @param callback - Optional callback function to be called after the animation completes
+     */
     const smoothScrollTo = (element: HTMLElement, scrollLeft: number, duration: number, callback?: () => void) => {
         const start = element.scrollLeft
         const change = scrollLeft - start
@@ -167,19 +111,50 @@
         animateScroll()
     }
 
+    /**
+     * Easing function for smooth scrolling animation
+     *
+     * @param t - Current time
+     * @param b - Start value
+     * @param c - Change in value
+     * @param d - Duration
+     * @returns The eased value
+     */
     const easeOutQuad = (t: number, b: number, c: number, d: number) => {
         t /= d
         return -c * t * (t - 2) + b
     }
 
-    onMounted(() => {
+    /**
+     * Update the container width and scroll position on mounted and window resize
+     */
+    const updateContainerWidth = () => {
         if (container.value) {
             state.containerWidth = container.value.offsetWidth
+            state.maxScrollPosition = container.value.scrollWidth - container.value.clientWidth
+
+            // Recalculate scroll position to stay within the new maximum scroll position
+            state.scrollPosition = Math.max(0, Math.min(state.scrollPosition, state.maxScrollPosition))
         }
+    }
+
+    onMounted(() => {
+        updateContainerWidth()
+
+        window.addEventListener('resize', updateContainerWidth)
     })
 
+    onBeforeUnmount(() => {
+        window.removeEventListener('resize', updateContainerWidth)
+    })
+
+    /**
+     * Whether the container can be scrolled to the left
+     */
     const canScrollLeft = computed(() => state.scrollPosition > 0 && container.value)
-    const canScrollRight = computed(
-        () => state.scrollPosition < (container.value?.scrollWidth ?? 0) - (container.value?.clientWidth ?? 0)
-    )
+
+    /**
+     * Whether the container can be scrolled to the right
+     */
+    const canScrollRight = computed(() => state.scrollPosition < state.maxScrollPosition)
 </script>
