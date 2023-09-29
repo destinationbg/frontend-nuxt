67a5578e925749aa106a0725f8eb122caee6b1d5
XenBG <wildbeastbg@gmail.com>
2023-09-30 02:01:55 +0300

    tweak: location history link styled

--- composables/useContentLinkHandler.ts
+++ composables/useContentLinkHandler.ts
@@ -5,12 +5,10 @@ function useContentLinkHandler() {
         const regex = /<a href="(https?:\/\/|\/)?(.*?)">([^<]+)<\/a>/g
         const replacedHtml = html.replace(regex, (_: any, protocol: string, url: string, text: any) => {
             if (protocol === 'http://' || protocol === 'https://') {
-                console.log(protocol + url)
                 return `<a href="${protocol}${url}" target="_blank" rel="noopener">${text}</a>`
             } else {
                 const newUrl = localePath('/' + url)
-                console.log('/' + url, newUrl)
-                return `<a href="${newUrl}" target="_blank" rel="noopener">${text}</a>`
+                return `<a href="${newUrl}">${text}</a>`
             }
         })
 
